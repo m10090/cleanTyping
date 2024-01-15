449601338a47e5f98afe05cac4b7e856d0537758
m10090 <tammwy22@gmail.com>
2024-01-15 11:59:45 +0200

    push omar

--- src/result.tsx
+++ src/result.tsx
@@ -1,4 +1,4 @@
-export default function Result( { author, wpm }:{string, number},) {
+export default function Result( { author, wpm },) {
 
   return(<h1>
     {author} typed at {wpm} words per minute
